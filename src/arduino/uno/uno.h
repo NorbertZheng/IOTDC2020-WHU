f454586cb2bee357cdb9fa757a15d7073befb53a
fassial <2508440340@qq.com>
2020-04-28 03:25:05 +0800

    src: support web_req(get & post) in arduino/uno

---
+++ src/arduino/uno/uno.h
@@ -0,0 +1,11 @@
+#ifndef UNO_H
+#define UNO_H
+
+/*
+    This is the header of uno module
+*/
+
+// local header
+#include "./src/utils/web_req/web_req.h"
+
+#endif
---
+++ src/arduino/uno/uno.ino
@@ -0,0 +1,23 @@
+#include "uno.h"
+
+// WIFI param
+char *ssid     = "Tenda_69F8D8";
+char *password = "262621123";
+// host param
+char *host  = "192.168.0.105";
+int port    = 3000;
+String post_data = "post_data=123";
+
+void setup() {
+    Serial.begin(115200);
+    Serial.println();
+
+    // connect to WIFI
+    connect_WIFI(ssid, password);
+}
+
+
+void loop() {
+    print_POST(host, port, post_data);
+    delay(5000);
+}
